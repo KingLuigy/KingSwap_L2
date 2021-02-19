09b1a537c5cd92a2a961257a5db6c602f9ba72f2
Vadim Konstantinov <vadim.konstantinov@gmail.com>
2021-02-19 15:42:24 +0300

    Proxy addresses updated

--- scripts/shared/addresses.kovan-sokol.js
+++ scripts/shared/addresses.kovan-sokol.js
@@ -9,28 +9,31 @@ module.exports = {
   xAmbAddress: '0xFe446bEF1DbF7AFE24E81e05BC8B271C1BA9a560',
 
   // AMB-extension contract (aka "mediator") for ERC20 on the layer1 chain
-  ambErc20ExtAddress: '0x4a493Af3cC8fb1385800ac92b3137DB20949194b', // the proxy
-  ambErc20ExtImplAddress: '0x5A312Ee76A421a9f88AF0581c6AFE3b313b6C358', // the implementation
+  ambErc20ExtAddress: '0x4a493Af3cC8fb1385800ac92b3137DB20949194b', // proxy
+  ambErc20ExtImplAddress: '0x5A312Ee76A421a9f88AF0581c6AFE3b313b6C358', // implementation
 
   // AMB-mediator contract (aka "mediator") for ERC20 on the side-chain
-  xAmbErc20ExtAddress: '0x15390f348e49135D06Ed6B9c968223aeD108E105', // the proxy
-  xAmbErc20ExtImplAddress: '0xe35973aee228883aE687830243b413470F665Aa7', // the implementation
+  xAmbErc20ExtAddress: '0x15390f348e49135D06Ed6B9c968223aeD108E105', // proxy
+  xAmbErc20ExtImplAddress: '0xe35973aee228883aE687830243b413470F665Aa7', // implementation
 
   // AMB-mediator contract (aka "mediator") for ERC1155 on the layer1 chain
-  ambErc1155ExtAddress: '0x0000000000000000000000000000000000000000', // ToDo: update
+  ambErc1155ExtAddress: '0x0000000000000000000000000000000000000000', // proxy // ToDo: update
+  ambErc1155ExtImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
+
   // AMB-mediator (aka "mediator") contract for ERC1155 on the side-chain
-  xAmbErc1155ExtAddress: '0x0000000000000000000000000000000000000000', // ToDo: update
+  xAmbErc1155ExtAddress: '0x0000000000000000000000000000000000000000', // proxy // ToDo: update
+  xAmbErc1155ExtImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 
   // $KING token on the layer1 chain (original token)
   kingTokenAddress: '0x0fc69EC9DB3643FFAF3164Ed9B486330C29B5A3c', // un-proxied
 
   // x$KING token on the side-chain (replica of the $KING)
-  xKingTokenAddress: '0xd15Ee89DD37E62d131e382c8df7911CE872bf74D', // the proxy
-  xKingTokenImplAddress: '0x6e7207B39791c44DB16D816669a675e86c195dC3', // the implementation
+  xKingTokenAddress: '0xd15Ee89DD37E62d131e382c8df7911CE872bf74D', // proxy
+  xKingTokenImplAddress: '0x6e7207B39791c44DB16D816669a675e86c195dC3', // implementation
 
   // ERC1155 on the layer1 chain (replica of the side-chain ERC1155)
   Erc1155Address: '0x0000000000000000000000000000000000000000', // ToDo: update
   // ERC1155 on the side-chain (original tokens)
-  xErc1155Address: '0x0000000000000000000000000000000000000000', // ToDo: update
-
+  xErc1155Address: '0x0000000000000000000000000000000000000000', // proxy // ToDo: update
+  xErc1155ImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 }
--- scripts/shared/addresses.mainnet-xdai.js
+++ scripts/shared/addresses.mainnet-xdai.js
@@ -8,29 +8,31 @@ module.exports = {
   xAmbAddress: '0x75Df5AF045d91108662D8080fD1FEFAd6aA0bb59',
 
   // AMB-extension contract (aka "mediator") for ERC20 on the layer1 chain
-  ambErc20ExtAddress: '0xD1843507c588e5D0e11413b29779cC97E14e0242', // the proxy // ToDo: update
-  ambErc20ExtImplAddress: '0x0000000000000000000000000000000000000000', // the implementation // ToDo: update
+  ambErc20ExtAddress: '0xD1843507c588e5D0e11413b29779cC97E14e0242', // proxy
+  ambErc20ExtImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 
   // AMB-mediator contract (aka "mediator") for ERC20 on the side-chain
-  xAmbErc20ExtAddress: '0x4c9c971fbEFc93E0900988383DC050632dEeC71E', // the proxy
-  xAmbErc20ExtImplAddress: '0x0000000000000000000000000000000000000000', // the implementation // ToDo: update
+  xAmbErc20ExtAddress: '0x4c9c971fbEFc93E0900988383DC050632dEeC71E', // proxy
+  xAmbErc20ExtImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 
   // AMB-mediator contract (aka "mediator") for ERC1155 on the layer1 chain
-  ambErc1155ExtAddress: '0x24DD13C6b54E1538061FE8cda14E0C7F27BE9363', // the proxy
-  ambErc1155ExtImplAddress: '0x0000000000000000000000000000000000000000', // the implementation // ToDo: update
+  ambErc1155ExtAddress: '0x24DD13C6b54E1538061FE8cda14E0C7F27BE9363', // proxy
+  ambErc1155ExtImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 
   // AMB-mediator (aka "mediator") contract for ERC1155 on the side-chain
-  xAmbErc1155ExtAddress: '0x0000000000000000000000000000000000000000', // ToDo: update
+  xAmbErc1155ExtAddress: '0x4EdC94bC630Ee57A7D3FFeDaa03498430B7353ED', // proxy
+  xAmbErc1155ExtImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 
   // $KING token on the layer1 chain (original token)
   kingTokenAddress: '0xd2057d71fe3f5b0dc1e3e7722940e1908fc72078', // un-proxied
 
   // x$KING token on the side-chain (replica of the $KING)
-  xKingTokenAddress: '0xcCd05d20Cc7f1994425Dd21A8939A222D433cD1C', // the proxy
-  xKingTokenImplAddress: '0x0000000000000000000000000000000000000000', // the implementation // ToDo: update
+  xKingTokenAddress: '0xcCd05d20Cc7f1994425Dd21A8939A222D433cD1C', // proxy
+  xKingTokenImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 
   // ERC1155 on the layer1 chain (replica of the side-chain ERC1155)
   Erc1155Address: '0x0000000000000000000000000000000000000000', // ToDo: update
   // ERC1155 on the side-chain (original tokens)
-  xErc1155Address: '0x0000000000000000000000000000000000000000', // ToDo: update
+  xErc1155Address: '0xe26e5c3297e00321683Ca5aB242A77bef947E3e9', // proxy
+  xErc1155ImplAddress: '0x0000000000000000000000000000000000000000', // implementation // ToDo: update
 }
